08445b309610b162e74e89a3dff77dc5e7622934
Haoliang Zhang <hlzhang26@outlook.com>
2022-12-03 14:05:32 -0700

    fix signUp page event handler

--- Client/Public/signup.js
+++ Client/Public/signup.js
@@ -1,59 +1,62 @@
+const handleSignup = async (event) => {
+  event.preventDefault();
+  const formValue = {
+    email: document.getElementById("emailID").value,
+    password: document.getElementById("password").value,
+    repeatPassword: document.getElementById("repeatPassword").value,
+  };
 
+  const formDataValidated = validateSignup(formValue);
 
-
-const handleSignup = async () => {
-    const formValue = {
-        email: document.getElementById('emailID').value,
-        passowrd: document.getElementById('password').value,
-        repeatPassword: document.getElementById('repeatPassword').value,
-    }
-    const forDataValidated = validateSignup(formValue)
-
-    if(formDataValidated) {
-    
-    const response = await fetch('/signup', {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc. 
+  if (formDataValidated) {
+    const response = await fetch("/signup", {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(formData) 
+      body: JSON.stringify(formValue),
     });
-    console.log(response)
-    if(response.status !== 200) {
-        const responseBody = await response.json()
-        console.log(responseBody)
-        showError(responseBody.error)
+    console.log(response);
+    if (response.status === 200) {
+      console.log("happy path");
+      window.location.assign("/leggings");
+    } else {
+      const responseBody = await response.json();
+      console.log(responseBody);
+      showError(responseBody.error);
     }
   }
-}
+};
 
 const validateSignup = (formValue) => {
+  if (!formValue.email || formValue.email === "") {
+    return false;
+  }
 
-    if( (!formValue.email || formValue.email === "")) {
+  if (!formValue.password || formValue.password === "") {
+    return false;
+  }
 
-    }
+  if (!formValue.repeatPassword || formValue.repeatPassword === "") {
+    return false;
+  }
 
-    if( (!formValue.password || formValue.password === "")) {
+  if (!formValue.email.includes("@")) {
+    return false;
+  }
 
-    }
-
-    if( (!formValue.repeatPassword || formValue.repeatPassword === "")) {
-
-    }
-    
-    if (!formValue.email.includes('@')) {
-        
-    }
-
-    if(formValue.password === formValue.repeatPassword) {
-
-    }
-}
+  if (formValue.password !== formValue.repeatPassword) {
+    return false;
+  }
+  return true;
+};
 
 const showError = (errorMessage) => {
-    const body = document.getElementsByTagName('body')[0]
+  const body = document.getElementsByTagName("body")[0];
 
-    body.insertAdjacentHTML('beforeend', `
+  body.insertAdjacentHTML(
+    "beforeend",
+    `
     <div class="toast align-items-center text-bg-primary border-0" role="alert" aria-live="assertive" aria-atomic="true">
     <div class="d-flex">
       <div class="toast-body">
@@ -64,17 +67,17 @@ const showError = (errorMessage) => {
             aria-label="Close" onClick=""></button>
     </div>
   </div>  
-</div>`)
-}
+</div>`
+  );
+};
 
 const closeError = () => {
-    const toast = document.getElementById
-}
+  const toast = document.getElementById;
+};
 
-const signupButton = document.getElementById("signupBtn")
-signupButton.addEventListener('click', handleSignup),
-
-showError('Sample Error Message')
+const signupButton = document.getElementById("signUpBtn");
+signupButton.addEventListener("click", handleSignup),
+  showError("Sample Error Message");
 
 // const inputElement = document.getElementById('emailId');
 
@@ -101,6 +104,3 @@ showError('Sample Error Message')
 // inputElement.onfocus = userSelectsInputElement
 
 // inputElement.addEventListener('focusout', checkEmail)
-
-
-
