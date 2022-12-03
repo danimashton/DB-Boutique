d569d3f2ee80f1a477b46ccbc1268b66a74d8db2
Haoliang Zhang <hlzhang26@outlook.com>
2022-12-03 14:04:34 -0700

    update MongoDB connection uri

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,20 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 let connection = undefined;
 
 const getConnection = async () => {
-    if(connection) {
-        console.log('returning existing connection')
-        return connection
-    } else {
-        console.log('creating new connection')
-        connection = await mongoose.connect('mongodb+srv://danimashton:<password>@cluster0.odqunjl.mongodb.net/?retryWrites=true&w=majority')
-        return connection;
-    }
-}
-
-module.exports = {
-    getConnection,
-    mongoose,
-    Schema: mongoose.Schema
+  if (connection) {
+    console.log("returning existing connection");
+    return connection;
+  } else {
+    console.log("creating new connection");
+    connection = await mongoose.connect(
+      "mongodb+srv://danimashton:oAjHXesVPgmFp2r1@cluster0.odqunjl.mongodb.net/dbboutique?retryWrites=true&w=majority/"
+    );
+    return connection;
+  }
 };
 
+module.exports = {
+  getConnection,
+  mongoose,
+  Schema: mongoose.Schema,
+};
